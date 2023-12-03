54aa303ba8f81610166fa8eb709807e5a378cb7c
moneypiaorui <2451879312@qq.com>
2023-12-03 23:59:28 +0800

    1

--- web/js/chat.js
+++ web/js/chat.js
@@ -1,25 +1,35 @@
 let chatInput = document.querySelector(".chat-input-area input");
 let chatCardContainor = document.querySelector(".chat-show-area");
-document.querySelector(".chat-input-area button").addEventListener("click", () => {
-    socket.send(JSON.stringify({
-        state: "inRoom",
-        type: "chatMessage",
-        message:chatInput.value
-      }))
-      chatInput.value="";
-});
 
 function addChatCard(data) {
     let card = document.createElement("div")
     card.classList.add("chat-show-card");
-    if(data.isSys==0){
-        card.innerText = data.name+":"+data.message;
+    if (data.isSys == 0) {
+        card.innerText = data.name + ":" + data.message;
     }
     chatCardContainor.appendChild(card);
-    chatCardContainor.scrollTop=chatCardContainor.scrollHeight;//滚轮自动滑到底部
+    chatCardContainor.scrollTop = chatCardContainor.scrollHeight;//滚轮自动滑到底部
 }
 
-chatCardContainor.addEventListener("click",()=>{
+document.querySelector(".chat-input-area button").addEventListener("click", () => {
+    socket.send(JSON.stringify({
+        state: "inRoom",
+        type: "chatMessage",
+        message: chatInput.value
+    }))
+    chatInput.value = "";
+});
+chatInput.addEventListener("keydown",event=>{
+    if(event.keyCode ==13){
+        socket.send(JSON.stringify({
+            state: "inRoom",
+            type: "chatMessage",
+            message: chatInput.value
+        }))
+        chatInput.value = "";
+    }
+})
+chatCardContainor.addEventListener("click", () => {
     document.querySelector(".chat-area").classList.toggle("folded")
     // document.querySelector(".chat-input-area").classList.toggle("hide")
 })
